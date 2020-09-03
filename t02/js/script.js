ab3cbcb8e1eb15adbb3315fd833684d84b0f3f2a
demetreus420 <dima.net.dd@gmail.com>
2020-09-03 15:02:50 +0300

    Task02 finished

---
+++ t02/js/script.js
@@ -0,0 +1,11 @@
+function copyObj(obj) {
+    return Object.assign({}, obj);
+}
+
+// const user = {
+//     name: "Steve",
+//     surname: "Rogers",
+//     age: 101,
+//     city: "New York" };
+
+// console.log(copyObj(user));
